fe7e19e5484753b5c153d434f0fb4c9ca3aaf967
son0p <fede2001@gmail.com>
2020-03-17 14:08:54 -0500

    se usa jsonlite

--- analisis_covid19_mde.R
+++ analisis_covid19_mde.R
@@ -1,15 +1,16 @@
+rm(list=ls())
 if(!require(plotly)){install.packages("plotly")}
 if(!require(lubridate)){install.packages("lubridate")}
 if(!require(htmlwidgets)){install.packages("htmlwidgets")}
-install.packages("rjson")
+install.packages("jsonlite")
 
 
-library("rjson")
+library("jsonlite")
 json_file <- "https://pomber.github.io/covid19/timeseries.json"
-json_data <- fromJSON(file=json_file)
+json_data <- fromJSON(json_file)
 colombia_data <- json_data$Colombia
-
-p <- plot_ly(  x = colombia_data[[55]]$date, y = colombia_data[[55]]$confirmed, type ='bar', color = I("plum4") )%>%
-    layout(yaxis = list(title = 'Confirmados Colombia'))
+colombia_data$date <- ymd(colombia_data$date)
+p <- plot_ly(  x = colombia_data$date, y = colombia_data$confirmed, type ='bar', color = I("plum4") )%>%
+    layout(yaxis = list(title = 'Confirmados Colombia COVID 19'))
     htmlwidgets::saveWidget(as_widget(p), "/tmp/HoursPerWeek.html")
 ggplotly(p)
