b1a62839f5353a8ace25eaef9cc5d93722338577
brolin <brolin108@gmail.com>
2020-03-18 18:49:32 -0500

    Agrega la posibilidad de consultar los datos directamente al instituto nacional de salud

--- analisis_covid19_mde.R
+++ analisis_covid19_mde.R
@@ -3,6 +3,7 @@ if(!require(plotly)){install.packages("plotly")}
 if(!require(lubridate)){install.packages("lubridate")}
 if(!require(htmlwidgets)){install.packages("htmlwidgets")}
 if(!require(jsonlite)){install.packages("lsonlite")}
+if(!require(purrr)){install.packages("purrr")}
 
 
 json_file <- "https://pomber.github.io/covid19/timeseries.json"
@@ -25,3 +26,18 @@ pm <- plot_ly(  x = compare_data$date, y = compare_data$confirmados_col, type ='
     layout(yaxis = list(title = 'Confirmados compare  COVID 19'), barmode = 'group')
 htmlwidgets::saveWidget(as_widget(p), "/tmp/covid19_w.html")
 ggplotly(pm)
+
+##
+## Datos de instituto nacional de salud
+##
+data <- fromJSON("https://e.infogram.com/api/live/flex/0e44ab71-9a20-43ab-89b3-0e73c594668f/dfee1a5c-5cc8-4e90-8efb-d5bdf2803bf6?")
+ninfectados <- dim(data$data)[2]
+
+infectados_df <- map(1:ninfectados, function(x) {
+    data$data[1,x,]
+}) %>% unlist(.) %>% matrix(.,nrow=ninfectados, byrow = TRUE) %>% data.frame(., stringsAsFactors = FALSE)
+
+colnames(infectados_df) <- infectados_df[1,]
+infectados_df <- infectados_df[-1,]
+
+infectados_df
